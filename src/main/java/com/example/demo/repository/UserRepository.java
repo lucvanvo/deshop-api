eeec963707732c4c8b18eca89680e7aafcbceb2c
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2025-04-05 22:12:37 +0700

    #24 remove unnecessary annotation

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, String> {
-	
+	public Optional<User> findByEmail(String email);
 }
