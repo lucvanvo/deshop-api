807d06a16e3d15859116552c1bd24b4955a17f7c
DESKTOP-7NKT9DQ\Huy <vanhuyvo1997.hv@gmail.com>
2025-04-09 21:57:29 +0700

    #4 handle exceptions

--- src/main/java/com/example/demo/controller/GlobalControllerAdvice.java
+++ src/main/java/com/example/demo/controller/GlobalControllerAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.demo.exception.ResourceExistedException;
 import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.exception.auth.UnauthorizedException;
 
 @ControllerAdvice
 
@@ -20,5 +21,20 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
         return ResponseEntity.status(404).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        return ResponseEntity.status(500).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(400).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<String> handleUnauthorizedException(UnauthorizedException ex) {
+        return ResponseEntity.status(401).body(ex.getMessage());
     }
 }
